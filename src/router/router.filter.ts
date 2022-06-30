ea8992af70a3887bb8fcf9e13a6ef2b99249adeb
lvzl <lvzl@yusys.com.cn>
2022-06-30 13:16:28 +0800

    fix:路由loading效果

--- src/router/router.filter.ts
+++ src/router/router.filter.ts
@@ -1,10 +1,15 @@
 import { useMainStore } from '@/store'
 import { Router } from 'vue-router'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css' // progress bar style
 // import { useMainStore } from '@/store/index'
+
+nprogress.configure({ showSpinner: true }) // NProgress 配置
 
 const { log } = console
 const initRouterFilter = (router: Router) => {
   router.beforeEach((to, from, next) => {
+    nprogress.start()
     // const mainStore = useMainStore()
     log('前置守卫被调用')
     next()
@@ -20,6 +25,7 @@ const initRouterFilter = (router: Router) => {
   })
 
   router.afterEach(() => {
+    nprogress.done()
     log('全局后置钩子')
   })
 }
